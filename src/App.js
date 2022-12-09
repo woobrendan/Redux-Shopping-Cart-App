0734cc7dc66a6e2a5d279cbf9d6d9edf5a2c4b72
Brendan Woo <woo.brendan@gmail.com>
2022-12-09 12:22:58 -0800

    import useSelectore to get state isLoggedIn. conditionally render auth or layout based on state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {!isLoggedIn ? <Auth /> : <Layout />}
+      {/* {<Auth />}
+      <Layout /> */}
     </div>
   );
 }
