9a6cd46971b0093644c8503dc3f7c9e02f90d63f
Brendan Woo <woo.brendan@gmail.com>
2022-12-09 12:10:33 -0800

    create slice file for authentication. give name, initial state, reducers login and logout. export authSlice actions and authslice

---
+++ src/store/auth-slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { isLoggedIn },
+  reducers: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const AuthActions = authSlice.actions;
+
+export default authSlice;
